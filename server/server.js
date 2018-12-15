89df1053cf4a80dd81bb3b46cda977f07e2c796b
paulbriar <pbriar@live.com>
2018-12-15 11:39:38 -0400

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todos');
@@ -26,6 +27,23 @@ app.get("/todos", (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
     }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.get("/todos/:id", (req, res) => {
+    let id = req.params.id;        
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    };
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return console.log('ID not found');
+        }
+        res.send({todo});
+    }).catch((err) => {
         res.status(400).send(err);
     });
 });
