04ba7cc19d8ff06f51b83f8e952fc07d8763e58e
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2018-12-18 22:12:28 -0400

    Setup seperate test & dev ENV's

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 'use strict';
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,7 +11,7 @@ let {Todo} = require('./models/todos');
 let {User} = require('./models/user');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
